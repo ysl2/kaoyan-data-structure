5d3174ba474e4befe454ea09dfc3bbba85cf2d45
ysl970629 <1097985743@qq.com>
2020-08-31 17:41:49 +0800

    2020-08-31

--- 01-王道课后题-大题/第2章/2.3/T17.c
+++ 01-王道课后题-大题/第2章/2.3/T17.c
@@ -1,6 +1,6 @@
 int symmetry(DLinkList L) {
     DLinkNode *p = L->next, *q = L->prior;
-    while (p != q && p->next != q) {
+    while (p != q && p->next != q) {  // 分别对应奇数和偶数个结点
         if (p->data == q->data) {
             p = p->next;
             q = q->prior;
--- 01-王道课后题-大题/第2章/2.3/T3.c
+++
@@ -1,7 +0,0 @@
-void recursive(LinkList L) {
-    if (L->next != NULL) {
-        recursive(L->next);
-    }
-    visit(L->data);
-}
-
