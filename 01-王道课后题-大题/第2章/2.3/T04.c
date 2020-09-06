c0cb96a0b68280009e869d53b050d85df4990eff
ysl970629 <1097985743@qq.com>
2020-09-06 17:06:39 +0800

    2020-09-06

--- 01-王道课后题-大题/第2章/2.3/T04.c
+++ 01-王道课后题-大题/第2章/2.3/T04.c
@@ -1,31 +1,10 @@
-// 2020-09-02 第二次修订
-void deleteMin(LinkList &L) {
-// 本题中，p和minp并不是必须要定义的。但是pre和minpre是必须要有的。
-// 原因是对链表操作必须获取前驱节点，否则在删除结点后无法连接
-    if (L == NULL || L->next == NULL) return ;
-    LinkNode *pre = L, *p = L->next;
-    LinkNode *minpre = L, *minp = L->next;
-    while (p != NULL) {
-        if (p->data < minp->data) {
-            minp = p;
-            minpre = pre;
-        }
-        pre = p;
-        p = p->next;
-    }
-    delete(L, pre);
-}
-
-void delete(LinkList &L, LinkNode *&pre) {
-    LinkNode *temp = pre->next;
-    pre->next = pre->next->next;
-    free(temp);
-}
+// 2020-09-06 第三次修订
 
 -------------------------------------------
 void deleteMin(LinkList &L) {
-    LinkNode *pre = L, *p = L->next;
-    LinkNode *minpre = pre, *minp = p;
+	if (L == NULL) return ;
+    LinkNode *p = L->next, *pre = L;
+    LinkNode *minp = L->next, *minpre = L;
     while (p != NULL) {
         if (p->data < minp->data) {
             minp = p;
--- 01-王道课后题-大题/第2章/2.3/T17.c
+++ 01-王道课后题-大题/第2章/2.3/T17.c
@@ -1,5 +1,30 @@
-int symmetry(DLinkList L) {
-    DLinkNode *p = L->next, *q = L->prior;
+// 2020-09-06 第二次修订
+typedef int ElemType;
+typedef struct DCLinkNode {
+    ElemType data;
+    struct DCLinkNode *prior, *next;
+} DCLinkNode, *DCLinkList;
+
+DCLinkList L;
+
+bool isSym(DCLinkList &L) {  // Double Circle LinkList
+    DCLinkNode *p = L->next;
+    DCLinkNode *q = L->prior;
+    // L如果是奇数个结点，最终会判断p == q
+    // L如果是偶数个结点，最终会判断p->next == q
+    // 但上述两个情况不会同时发生，因此写在一起不会发生冲突
+    for ( ; p->data == q->data && (p->next != q && p != q) ; p = p->next, q = q->prior);
+    // 和上面一样，只要奇数或者偶数满足其中一种情况即可
+    // 无论哪种情况，都必须保证整个链表遍历完整，而不是因为有不相等的元素导致退出循环
+    // 但是一旦有不相等的元素，必然会导致链表遍历不完整，也就是两个指针没碰头
+    // 除了一种情况，就是链表偶数的时候，
+    // 遍历到最后的位置时，两个指针刚好碰头，但是不能保证这两个data一定相等
+    // 所以需要在最终的判断里体现出来这一个信息：
+    return (p->next == q && p->data == q->data) || p == q;
+}
+-------------------------------------------------------------------------
+int symmetry(DCLinkList L) {
+    DCLinkNode *p = L->next, *q = L->prior;
     while (p != q && p->next != q) {  // 分别对应奇数和偶数个结点
         if (p->data == q->data) {
             p = p->next;
