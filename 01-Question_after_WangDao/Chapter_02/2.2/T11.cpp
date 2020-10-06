b7488e420982c910cc2b56c4ddc406fed33e4cc8
ysl970629 <1097985743@qq.com>
2020-10-06 13:51:41 +0800

    2020-10-06

--- 01-Question_after_WangDao/Chapter_02/2.2/T11.cpp
+++ 01-Question_after_WangDao/Chapter_02/2.2/T11.cpp
@@ -16,11 +16,11 @@ double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
         return findMedianSortedArrays(nums2, nums1);
 
     // ci 为第i个数组的割,比如c1为2时表示第1个数组只有2个元素。LMaxi为第i个数组割后的左元素。RMini为第i个数组割后的右元素。
-    int LMax1, LMax2, RMin1, RMin2, c1, c2, lo = 0, hi = 2 * n;  //我们目前是虚拟加了'#'所以数组1是2*n长度
+    int LMax1, LMax2, RMin1, RMin2, c1, c2, low = 0, high = 2 * n;  //我们目前是虚拟加了'#'所以数组1是2*n长度
 
     //二分
-    while (lo <= hi) {
-        c1 = (lo + hi) / 2;  //c1是二分的结果
+    while (low <= high) {
+        c1 = (low + high) / 2;  //c1是二分的结果
         c2 = m + n - c1;
 
         LMax1 = (c1 == 0) ? INT_MIN : nums1[(c1 - 1) / 2];
@@ -29,9 +29,9 @@ double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
         RMin2 = (c2 == 2 * m) ? INT_MAX : nums2[c2 / 2];
 
         if (LMax1 > RMin2)
-            hi = c1 - 1;
+            high = c1 - 1;
         else if (LMax2 > RMin1)
-            lo = c1 + 1;
+            low = c1 + 1;
         else
             break;
     }
